837d77086f6f1e05aab9365563c71b61342664be
Jiislam <jihadul.islam9043@gmail.com>
2023-07-31 13:26:38 +0600

    condditional

--- comparison.js
+++ comparison.js
@@ -1,23 +1,40 @@
-console.log(5 < 6);
-console.log(5 > 6);
-console.log(5 == 6);
-console     .log(6 == 6);
-console.log(7 != 6);
-console.log(5 <= 6);
-console.log(5 >= 6);
+// // console.log(5 < 6);
+// // console.log(5 > 6);
+// // console.log(5 == 6);
+// // console     .log(6 == 6);
+// // console.log(7 != 6);
+// // console.log(5 <= 6);
+// // console.log(5 >= 6);
 
 
-// variable comparison.
+// // // variable comparison.
 
-var num2 = 78;
-var num1 = 45;
-console.log(num2 < num1);
-console.log(num2 > num1);
-console.log(num2 === num1);
-console.log(num2 >= num1);
-console.log(num2 <= num1);
+// // var num2 = 78;
+// // var num1 = 45;
+// // console.log(num2 < num1);
+// // console.log(num2 > num1);
+// // console.log(num2 === num1);
+// // console.log(num2 >= num1);
+// // console.log(num2 <= num1);
 
-// multiple comaprisonm
+// // // multiple comaprisonm
 
-money1 > money2 && result1 > result2 && height1 > height2
-moneyt1> money2 || result1> result2 || height1 > height2
+// // money1 > money2 && result1 > result2 && height1 > height2
+// // moneyt1> money2 || result1> result2 || height1 > height2
+
+// // var money = 100;
+// // var coffee =20;
+// // var dairymilk: 150;
+
+// // if{
+// //     money > coffee 
+// // }
+
+
+// var iphonePrice = 95000;
+// var myBudget = 950000;
+
+// // if I phone price is less than myBudget, we will buy iphone
+
+// if(iphonePrice < myBudget){ console.log('Iphone kinbo');
+// }
---
+++ conditional.js
@@ -0,0 +1,19 @@
+var chickenPrice = 40;
+var myMoney = 50;
+if (chickenPrice > myMoney) {console.log(' We cant buy chicken');}
+if (chickenPrice <= myMoney) {console.log(' We can buy chicken');}
+
+//  if chicken price less than my budget, 
+// option is chicken
+// else 
+// 
+if (chickenPrice < myMoney) {
+    console.log('I will eat chicken');
+}
+
+else{
+    console.log('I will eat potato');
+}
+
+
+
